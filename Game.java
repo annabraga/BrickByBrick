b45be425cfcf42fcd93b454926ef5dbab50b6d0a
Thor <thor.fc21@gmail.com>
2015-05-26 19:57:15 -0300

    Initial commit

---
+++ Game.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+class Game{
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("BrickByBrick");
+		frame.setSize(1920, 800);  
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
